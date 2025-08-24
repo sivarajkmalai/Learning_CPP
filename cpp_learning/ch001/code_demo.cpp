0da148f2f42f53236a3615d4328a4408e4126300
SIVARAJ K <141554772+sivarajkmalai@users.noreply.github.com>
2025-08-24 17:01:31 +0000

    chapter 1  update version 0.0.1

---
+++ cpp_learning/ch001/code_demo.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <string>
+using  namespace std ;
+
+int main(){
+    string name ;
+    cout << "Enter your Name: " << flush ;
+    cin >> name ;
+    cout << "Hi !, There " << name << " Welcome to the cpp Learning course" << endl ;
+
+    cout << endl << endl;
+    return 0 ; 
+}
