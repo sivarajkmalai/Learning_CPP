727aa8b3932c04ebd477ce655692e27685bc42bf
SIVARAJ K <141554772+sivarajkmalai@users.noreply.github.com>
2025-08-27 13:06:35 +0000

    Launch update

--- cpp_learning/ch03/Pointer.cpp
+++ cpp_learning/ch03/Pointer.cpp
@@ -3,5 +3,24 @@
 using namespace std ;
 
 int main(){
-    
+    /*
+     *  # Pointers   - Holds Memory Address
+     *  
+     */
+
+    int a = 37 ;
+
+    int *ptr ;
+
+    ptr = &a ;
+
+    cout << " Content of a ==    " << a << endl ;   
+    cout << " Address of a ==    "  <<  &a << endl ;
+    cout << " ptr is pointing to address ==    "  <<  ptr << endl ;
+    cout << "where ptr is  pointing , we have ==     "  <<  *ptr << endl ;
+    cout << "Address of ptr ==           "  <<  &ptr << endl ;
+    cout << endl << endl ;
+    return 0 ;
+
+
 }
